d11392175fe7bb7e3dc836cadbe9e4994e9a2d14
mohanmavis <mohanaraj444.gmail.com>
2025-11-25 08:11:59 +0530

    Toggle Theme Complete

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { ThemeToggle } from "../components/ToggleTheme";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+      {/* Theme Toggle */}
+      <p>Hello World</p>
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+    </div>
+  );
+};
