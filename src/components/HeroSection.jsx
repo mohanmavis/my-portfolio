b5d7bbda56e825b8d42e4aede19f1ce1f5135686
mohanmavis <mohanaraj444.gmail.com>
2025-11-27 10:09:54 +0530

    HeroSection Complete

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,35 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="home"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4 "
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6  ">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight ">
+            <span className="opacity-0 animate-fade-in mr-1"> Hi, I'm</span>
+            <span className="text-primary opacity-0  animate-fade-in-delay-1 ">
+              {" "}
+              Mohan
+            </span>
+            <span className="text-gradient ml-1 opacity-0 animate-fade-in-delay-2 ">
+              {" "}
+              Raj
+            </span>
+          </h1>
+          <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3  ">
+            Building the future, one artifact at a time. Welcome to The Mavis
+            Archive.
+          </p>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce ">
+        <span className="text-sm text-muted-foreground mb-2">scroll</span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import { ThemeToggle } from "../components/ToggleTheme";
 import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "@/components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
       {/* Theme Toggle */}
-      <p>Hello World</p>
+
       <ThemeToggle />
       {/* Background Effects */}
       <StarBackground />
       {/* Navbar */}
-
+      <Navbar />
       {/* Main Content */}
+      <main>
+        <HeroSection />
+      </main>
     </div>
   );
 };
