1f5f4b88d11f5337caa8753c183120f240df25eb
mohanmavis <mohanaraj444.gmail.com>
2025-12-02 15:22:25 +0530

    Configure vite build for docs folder

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-portfolio/",
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
